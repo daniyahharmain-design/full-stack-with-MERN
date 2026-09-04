5d0c22ba20394872bfd8fb3044e23ef1f804f0f5
Daniyah Harmain <daniyahharmain@gmail.com>
2026-09-04 16:25:47 +0530

    javascript day10

--- Javascript/DAY 10/day 10/script.js
+++ Javascript/DAY 10/day 10/script.js
@@ -1,3 +1,48 @@
-document.getElementById("someId")
+let heading = document.getElementById("mainHeading");
+console.log(heading);
 
-console.log(document.getElementById("mainHeading"))
+console.dir(heading)
+
+
+let intro = document.getElementById("intro");
+console.log(intro);
+
+
+let skill = document.getElementById("skillsList")
+
+console.log(skill)
+
+
+
+let pic = document.getElementById("gallery")
+ console.log(pic)
+
+
+ let footer = document.getElementById("pageFooter")
+ console.log(footer)
+
+
+
+//  task 2 select element by class name
+let para =document.getElementsByClassName("description")
+console.log(para)
+
+let skills =document.getElementsByClassName("skill")
+console.log(skills)
+
+let projetimg =document.getElementsByClassName("project")
+console.log(projetimg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
